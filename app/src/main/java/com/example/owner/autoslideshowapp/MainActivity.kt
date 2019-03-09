ad3fdf937aa0a843b319e2152898f746610c58fd
gikogiko <blend8e@gmail.com>
2019-03-09 21:58:17 +0900

    lesson5 課題

--- app/src/main/java/com/example/owner/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/com/example/owner/autoslideshowapp/MainActivity.kt
@@ -8,8 +8,10 @@ import android.database.Cursor
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.provider.MediaStore
 import android.util.Log
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Long.getLong
 import java.util.*
@@ -18,6 +20,11 @@ class MainActivity : AppCompatActivity() {
 
     private val PERMISSIONS_REQUEST_CODE = 100
     private var cursor: Cursor? = null
+    private var mTimer: Timer? = null
+
+    // タイマー用の時間のための変数
+    private var mTimerSec = 0.0
+    private var mHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,24 +36,12 @@ class MainActivity : AppCompatActivity() {
             if (checkSelfPermission(permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 // 許可されている
                 getContentsInfo()
-                //進むボタン押下時
-                foward_button.setOnClickListener {
-                    fowardrContentsInfo()
-                }
-
-                //再生/一時停止ボタン押下時
-                play_pause_button.setOnClickListener {
-
-                }
-
-                //戻るボタン押下時
-                back_button.setOnClickListener {
-                    backContentsInfo()
-                }
-
             } else {
                 // 許可されていないので許可ダイアログを表示する
                 requestPermissions(arrayOf(permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
+                foward_button.setEnabled(false)
+                play_pause_button.setEnabled(false)
+                back_button.setEnabled(false)
             }
             // Android 5系以下の場合
         } else {
@@ -82,26 +77,72 @@ class MainActivity : AppCompatActivity() {
 
             imageView.setImageURI(imageUri)
         }
-        //cursor!!.close()
+        //進むボタン押下時
+        foward_button.setOnClickListener {
+            fowardContentsInfo()
+        }
+
+        //再生/一時停止ボタン押下時
+        play_pause_button.setOnClickListener {
+            if(mTimer==null){
+                foward_button.setEnabled(false)
+                back_button.setEnabled(false)
+                play_pause_button.text = "停止"
+                mTimer = Timer()
+                mTimer!!.schedule(object : TimerTask() {
+                    override fun run() {
+                        mHandler.post {
+                            fowardContentsInfo()
+                        }
+                    }
+                }, 2000, 2000) // 最初に始動させるまで 100ミリ秒、ループの間隔を 100ミリ秒 に設定
+            }else{
+                foward_button.setEnabled(true)
+                back_button.setEnabled(true)
+                play_pause_button.text = "再生"
+
+                mTimer!!.cancel()
+                mTimer = null
+            }
+        }
+
+        //戻るボタン押下時
+        back_button.setOnClickListener {
+            backContentsInfo()
+        }
+
     }
 
-    private fun fowardrContentsInfo() {
-        if (cursor!!.move(1)) {
+    private fun fowardContentsInfo() {
+        if (cursor!!.moveToNext()) {
             val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
             val id = cursor!!.getLong(fieldIndex)
-            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id + 1)
+            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+            imageView.setImageURI(imageUri)
+        }else if(cursor!!.moveToFirst()){
+            val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor!!.getLong(fieldIndex)
+            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
             imageView.setImageURI(imageUri)
         }
     }
 
     private fun backContentsInfo() {
-        if (cursor!!.move(-1)) {
+        if (cursor!!.moveToPrevious()) {
             val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
             val id = cursor!!.getLong(fieldIndex)
-            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id - 1)
+            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
             imageView.setImageURI(imageUri)
+        }else if(cursor!!.moveToLast()){
+            val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor!!.getLong(fieldIndex)
+            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+            imageView.setImageURI(imageUri)
+
         }
     }
 }
